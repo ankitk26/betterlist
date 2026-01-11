457e26b8b0b905d590d25e46ebf85fd429b7d962
ankitk26 <myselfankit51@gmail.com>
2026-01-11 21:05:12 +0530

    configure allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const devAllowedHost = mode === "development" ? [env.DEV_ALLOWED_HOST] : undefined;
+  
+  return {
   plugins: [
     tsconfigPaths(),
     tailwindcss(),
@@ -13,4 +17,8 @@ export default defineConfig({
     }),
     viteReact(),
   ],
+  server: {
+    allowedHosts: devAllowedHost,
+  }
+}
 });
