6928738d9476a983ad7cffd971041c7ac66bf59f
ankitk26 <myselfankit51@gmail.com>
2026-03-15 12:00:42 +0530

    update database

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,21 +1,10 @@
 import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { customSession } from "better-auth/plugins";
-import { db } from "~/db";
-import { account, session, user, verification } from "~/db/auth-schema";
+import { env } from "cloudflare:workers";
 import { getUserAccount } from "./get-user-account";
 
 export const auth = betterAuth({
-	database: drizzleAdapter(db, {
-		provider: "sqlite",
-		schema: {
-			user,
-			session,
-			account,
-			verification,
-		},
-	}),
-
+	database: env.DB,
 	session: {
 		expiresIn: 60 * 60 * 24 * 7, // 7 days
 		updateAge: 60 * 60 * 24, // 1 day (every 1 day the session expiration is updated)
